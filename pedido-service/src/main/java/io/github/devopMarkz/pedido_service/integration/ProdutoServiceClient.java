f8fc86a94d6a593b094726ce0928f84b67016c43
devopMarkz <marcosdev2002@gmail.com>
2025-06-10 00:59:12 -0300

    Criando metodos para o feignclient de Produto

--- pedido-service/src/main/java/io/github/devopMarkz/pedido_service/integration/ProdutoServiceClient.java
+++ pedido-service/src/main/java/io/github/devopMarkz/pedido_service/integration/ProdutoServiceClient.java
@@ -1,10 +1,24 @@
 package io.github.devopMarkz.pedido_service.integration;
 
+import io.github.devopMarkz.pedido_service.dto.ProdutoDTO;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @FeignClient(name = "produto-service", url = "http://localhost:8080")
 public interface ProdutoServiceClient {
 
+    @GetMapping("/produtos/{id}")
+    public ProdutoDTO findProdutoById(@PathVariable Long id);
 
+    @PutMapping("/estoque/reducao")
+    public void retirarProdutosDeEstoque(@RequestBody List<ProdutoDTO> produtos);
+
+    @PutMapping("/estoque/reposicao")
+    public void reporEstoque(@RequestBody List<ProdutoDTO> produtos);
 
 }
